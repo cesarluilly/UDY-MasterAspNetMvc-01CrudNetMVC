216d111fdf81fa067cc1754a4a18fb2adcaafce1
cesarluilly <cesarluilly.garcia@hotmail.com>
2024-04-20 20:45:57 -0600

    Video 22 controlador y metodo index

--- ProyectoCrudNet8MVC/CrudNet8MVC/Controllers/StudyInicioController.cs
+++ ProyectoCrudNet8MVC/CrudNet8MVC/Controllers/StudyInicioController.cs
@@ -1,12 +1,54 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CrudNet8MVC.Datos;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CrudNet8MVC.Controllers
 {
     public class StudyInicioController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _contexto;
+
+        //--------------------------------------------------------------------------------------------------------------
+        public StudyInicioController(
+                                                            
+                                                            //
+           ApplicationDbContext contexto
+           )
         {
-            return View();
+            _contexto = contexto;
+        }
+
+
+        //public IActionResult Index(
+
+        //    )
+        //{
+        //    return View(
+        //        //                              
+        //        this._contexto.Contacto.ToList()
+        //        );
+        //}
+        public async Task<IActionResult> Index(
+            //                                              //Nota. Tal parece que la aplicacion corre en un hilo
+            //                                              //    llamado PRINCIPAL(A), y es lo que capta las solicitudes, 
+            //                                              //    pero es aconsejable que ese hilo no se bloquee, 
+            //                                              //    y para eso se utiliza el async para que el hilo
+            //                                              //    principal no se bloquee y la aplicacion pueda
+            //                                              //    captar otras solicitudes, entonces la consulta
+            //                                              //    entonces la consulta esta corriendo en otro hilo(B)
+            //                                              //    y cuando se termina de ejectutar, entonces el hilo
+            //                                              //    principal continua su ejecucion despues de que si iso
+            //                                              //    dicha consulta.
+            //                                              //ENTONCES COMO CONCLUSION, EL HILO PRINCIPAL NUNCA DEBE
+            //                                              //    DE BLOQUEARSE PARA QUE PUEDA CAPTAR OTRAS SOLICITUDES.
+            //                                              //En caso de que el hilo principal llegara a bloquearse porque
+            //                                              //    la operacion tardara mucho, pero si le llega otra
+            //                                              //    peticion, por lo tanto la aplicacion no lo captaria
+            //                                              //    y entonces ahi va a ver una solicitud perdida.
+            )
+        {
+            return View(await this._contexto.Contacto.ToListAsync()
+                );
         }
     }
 }
--- ProyectoCrudNet8MVC/CrudNet8MVC/Program.cs
+++ ProyectoCrudNet8MVC/CrudNet8MVC/Program.cs
@@ -25,6 +25,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Inicio}/{action=Index}/{id?}");
+    pattern: "{controller=StudyInicio}/{action=Index}/{id?}");
 
 app.Run();
